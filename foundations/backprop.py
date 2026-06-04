a7f971991b87976e60bc81b38239d29c7b47c3e3
Benhao Huang <91367324+huskydoge@users.noreply.github.com>
2026-06-04 16:52:59 -0700

    Update Backpropagation (Build a Neural Net 2/5)

--- foundations/backprop.py
+++ foundations/backprop.py
@@ -1,24 +1,29 @@
 import numpy as np
 from numpy.typing import NDArray
+from typing import Tuple
 
 
 class Solution:
-    def forward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, activation: str) -> float:
+    def backward(self, x: NDArray[np.float64], w: NDArray[np.float64], b: float, y_true: float) -> Tuple[NDArray[np.float64], float]:
         # x: 1D input array
-        # w: 1D weight array (same length as x)
+        # w: 1D weight array
         # b: scalar bias
-        # activation: "sigmoid" or "relu"
+        # y_true: true target value
         #
-        # Pre-activation: z = dot(x, w) + b
-        # Sigmoid: σ(z) = 1 / (1 + exp(-z))
-        # ReLU: max(0, z)
-        # return round(your_answer, 5)
-        x = w @ x
-        x += b
+        # Forward: z = dot(x, w) + b, y_hat = sigmoid(z)
+        # Loss: L = 0.5 * (y_hat - y_true)^2
+        # Return: (dL_dw rounded to 5 decimals, dL_db rounded to 5 decimals)
 
-        if activation == "sigmoid":
-            x = 1 / (1 + np.exp(-x))
-        elif activation == "relu":
-            x = np.maximum(x, 0)
+        # dL / dy_hat = (y_hat - y_true)
+        # dy_hat / dz = y_hat * (1 - y_hat)
 
-        return np.round(x, 5)
+        # dz / dw  = x
+        # dz / db = 1
+
+        y_hat = 1 / (1 + np.exp(- (w @ x + b ))) # (bs, )
+
+        dL_dw = (y_hat - y_true) * y_hat * (1 - y_hat) * x
+        dL_db = (y_hat - y_true) * y_hat * (1 - y_hat)
+
+
+        return (np.round(dL_dw, 5), np.round(dL_db, 5))
